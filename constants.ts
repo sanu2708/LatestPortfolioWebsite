a3dd927d58c328a5eacb9f3b0173e61b458b8ae0
Sanu Singh <sanu27081@gmail.com>
2025-11-21 15:19:11 +0530

    First upload

--- constants.ts
+++ constants.ts
@@ -80,4 +80,4 @@ export const TESTIMONIALS: Testimonial[] = [
 // export const HERO_IMAGE = '/portrait.jpg';
 
 // Currently using Unsplash demo image:
-export const HERO_IMAGE = 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=800&auto=format&fit=crop';
+export const HERO_IMAGE = '/portrait.jpg';
